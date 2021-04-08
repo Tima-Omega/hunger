ca673c710be0bea4b05aceb503518b9d55e405c8
Tima-Omega <tima_omega@outlook.com>
2021-04-08 06:39:48 +0300

    Add Tabs

--- js/app.js
+++ js/app.js
@@ -3,3 +3,23 @@ $(document).ready(function() {
         minimumResultsForSearch: Infinity
     });
 });
+
+var swiper = new Swiper('.swiper-container', {
+    initialSlide: 1,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+});
+
+$(function() {
+	var tab = $('.tab__cont > div'); 
+	tab.hide().filter(':first').show(); 
+	
+	$('.tab__link').click(function(){
+		tab.hide(); 
+		tab.filter(this.hash).show(); 
+		$('.tab__link').removeClass('active');
+		$(this).addClass('active');
+		return false;
+	}).filter(':first').click();
+});
